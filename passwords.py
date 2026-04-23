8061d416f86f815573f3868457e65df9e6937edb
Raxim2011 <orazbaevraxim48@gmail.com>
2026-04-23 14:11:25 +0500

    Added password script

---
+++ passwords.py
@@ -0,0 +1,6 @@
+correct = ["111111", "1488", "Lox123", "otgrizy svoi yaytsa"]
+a = (input("Пароль! : "))
+if a in correct:
+    print("Добро Пожаловать, сударь!")
+else:
+    print("Что ты здесь делаешь?")
