09d49c0fd7bf63a1f62073db37ff5a0d74452148
Raxim2011 <orazbaevraxim48@gmail.com>
2026-04-23 14:22:49 +0500

    Modified passwords.py

--- passwords.py
+++ passwords.py
@@ -1,6 +1,13 @@
-correct = ["111111", "1488", "Lox123", "otgrizy svoi yaytsa"]
-a = (input("Пароль! : "))
-if a in correct:
-    print("Добро Пожаловать, сударь!")
-else:
-    print("Что ты здесь делаешь?")
+attempts = 0
+while True:
+    correct = ["111111", "1488", "abc123"]
+    a = (input("Пароль! : "))
+    if a in correct:
+        print("Добро Пожаловать, сударь!")
+        break
+    else:
+        print("Неверно! Попробуй ещё раз.")
+        attempts += 1
+        if attempts == 3:
+            print("Всё! Попытки закончились.")
+            break
