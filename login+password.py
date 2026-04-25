6ce3cb0789873fc2fb6de690e6555de6cc32f8ce
Raxim2011 <orazbaevraxim48@gmail.com>
2026-04-25 14:35:14 +0500

    New stones

---
+++ login+password.py
@@ -0,0 +1,10 @@
+# users = {
+#     "wempt": "111111",
+#     "admin": "1488",
+#     "kent": "abc123"
+# }
+
+# login = (input("Напиши логин: "))
+# if login in users:
+#     print("Логин существует!")
+# password =  
